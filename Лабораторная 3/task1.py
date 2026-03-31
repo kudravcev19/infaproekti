1b8a4771af70f8d17067f7beb4fd4ff1f6921811
kudravcev19 <kudryavtsev.mm@edu.spbstu.ru>
2026-03-31 16:15:38 +0300

    Лабораторная работа 3

---
+++ Лабораторная 3/task1.py
@@ -0,0 +1,14 @@
+def function(items, tovar):
+    for index, one_item in enumerate(items):
+        if one_item == tovar:
+            return index
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = function(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
