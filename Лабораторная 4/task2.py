b500b5af46e787c9c3e4edec81f55c6ad1ae5bc4
kudravcev19 <kudryavtsev.mm@edu.spbstu.ru>
2026-04-05 17:57:09 +0300

    Лабораторная работа 4

---
+++ Лабораторная 4/task2.py
@@ -0,0 +1,10 @@
+import json
+
+def task() -> float:
+    with open('input.json', 'r') as f:
+        data = json.load(f)
+
+    total = sum(item["score"] * item['weight'] for item in data)
+    return round(total, 3)
+
+print(task())
